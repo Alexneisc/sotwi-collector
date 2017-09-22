3cc328d5453f4e59241c6c4ba7d12f32ad3c2ef6
Alexey Neischenko <alexneisc@gmail.com>
2017-09-22 19:40:30 +0300

    Move pid file to tmp/pids

--- sotwi-collector-daemon.rb
+++ sotwi-collector-daemon.rb
@@ -1,3 +1,7 @@
 require 'daemons'
 
-Daemons.run('sotwi-collector.rb')
+Daemons.run(
+  'sotwi-collector.rb',
+  dir: File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'current','tmp', 'pids')),
+  dir_mode: :normal,
+)
